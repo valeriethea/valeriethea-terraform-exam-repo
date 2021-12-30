2f3f7ca8792393a1c436e9b6fc8edea087ae64aa
valeriethea <theaasuncion08@gmail.com>
2021-12-30 09:32:40 +0800

    Terraform Exam

--- variable.tf
+++ variable.tf
@@ -5,16 +5,4 @@ variable "public_key" {}
 variable "region" {
   default = "ap-southeast-1"
 }
-variable "network_address_space" {
-  default = "10.0.0.0/16"
-}
-variable "subnet_count" {
-  default = 3
-}
-variable "instance_count" {
-  default = 1
-}
-variable "common_tags" {
-  default = {}
-}
 variable "environment_tag" {}
